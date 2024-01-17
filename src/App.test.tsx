9e8ca34e1022e51e8b8f0a326df7002e66a59e18
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2024-01-17 09:29:08 +0700

    configurations (init): setup initial configuration (#1)
    - [x] vitest config
    - [x] signals configs
    - [x] install react-router-dom
    - [x] config global vitest
    - [x] config global jest-dom
    - [x] config code formatter vscode

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import App from "App";
+import { render, screen } from "@testing-library/react";
+
+describe("App.tsx", () => {
+  test("render", () => {
+    render(<App></App>);
+    const txt = screen.getByText("Blank page");
+    expect(txt).toBeInTheDocument();
+  });
+});

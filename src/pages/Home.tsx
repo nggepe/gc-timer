8defe45bf7cc076193b7b3ab9ef73d220223bb4e
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2024-01-23 23:05:54 +0700

    feat (ads)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,22 @@ import { Box, Typography } from "@mui/material";
 import BaseLayout from "../layouts/BaseLayout";
 import { _FC } from "../types/common";
 import Timers from "../components/timers/Timers";
+import { useEffect } from "react";
 
 const Home: _FC = () => {
+  useEffect(() => {
+    document.getElementById(
+      "google-ads"
+    )!.innerHTML = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2693207086553957"
+    crossorigin="anonymous"></script>`;
+  }, []);
   return (
     <BaseLayout>
       <Box sx={{ display: "block", width: "100%" }}>
         <Typography variant="h1">Multiple Timer</Typography>
       </Box>
       <Timers />
+      <div id="google-ads"></div>
     </BaseLayout>
   );
 };

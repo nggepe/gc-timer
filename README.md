28bb2b2ea7c46f5ca93bb528c2a3507f064d3730
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2024-01-23 10:14:21 +0700

    test deployment

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GC Timer
 
-An open source multiple timer
+Introducing our innovative Multiple Timer Application – a versatile solution for efficiently managing your time-sensitive tasks. With our user-friendly interface, you can effortlessly set and monitor multiple timers simultaneously, ensuring no deadline is missed or task overlooked. Boost your productivity by organizing your day with precision, thanks to the convenience of our Multiple Timer App. Never let time slip away – stay on top of your schedule effortlessly!
--- src/components/timers/timerFragments/TimerCountDown.tsx
+++ src/components/timers/timerFragments/TimerCountDown.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext, useEffect, useMemo } from "react";
+import { FC, useContext, useEffect, useMemo, useState } from "react";
 import { TimerContext } from "../../../contexts/TimerContext";
 import {
   Box,
@@ -8,7 +8,6 @@ import {
   Typography,
 } from "@mui/material";
 import { milliSecondsToTimer } from "../../../helpers/utils";
-import { getTimerById, updateTimerById } from "../../../storages/TimerStorage";
 import TIME_MOVE from "../../../constants/TIME_MOVE";
 
 const TimerCountDown: FC = () => {
@@ -17,14 +16,14 @@ const TimerCountDown: FC = () => {
     <Tooltip title="Countdown timer">
       <Button className="btn-block btn-text btn-no-padding">
         <Box display={"flex"} gap={"1rem"}>
-          <Typography fontSize={"32px"}>{_.distance.h}h</Typography>
-          <Typography fontSize={"32px"}>{_.distance.m}m</Typography>
-          <Typography fontSize={"32px"}>{_.distance.s}s</Typography>
-          <Typography fontSize={"32px"}>{_.distance.ms}ms</Typography>
+          <Typography fontSize={"32px"}>{_.distanceConvert.h}h</Typography>
+          <Typography fontSize={"32px"}>{_.distanceConvert.m}m</Typography>
+          <Typography fontSize={"32px"}>{_.distanceConvert.s}s</Typography>
+          <Typography fontSize={"32px"}>{_.distanceConvert.ms}ms</Typography>
         </Box>
         <LinearProgress
           variant="determinate"
-          value={(_.totalTimeSpent / _.totalTime) * 100}
+          value={((_.totalTime - _.distance) / _.totalTime) * 100}
         ></LinearProgress>
       </Button>
     </Tooltip>
@@ -34,46 +33,57 @@ const TimerCountDown: FC = () => {
 export default TimerCountDown;
 
 const useTimerCountDown = () => {
-  const { state, set } = useContext(TimerContext);
+  const { state } = useContext(TimerContext);
 
-  const distance = useMemo(() => {
-    return milliSecondsToTimer(state.totalTime - state.totalTimeSpent);
-  }, [state.totalTime, state.totalTimeSpent]);
+  const [distance, setDistance] = useState(
+    state.totalTime - state.totalTimeSpent - (Date.now() - state.lastStartAt)
+  );
+
+  const distanceConvert = useMemo(() => {
+    return milliSecondsToTimer(distance);
+  }, [distance]);
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout | null = null;
     if (state.isPlay)
       intervalId = setInterval(() => {
-        const timer = getTimerById(state.id);
-        if (!timer) {
-          return clearInterval(intervalId!);
-        }
-        const spent = timer.totalTimeSpent + TIME_MOVE;
-        updateTimerById(state.id, "totalTimeSpent", spent as never);
-
-        set({
-          ...timer,
-          totalTimeSpent: spent as never,
-        });
-
-        if (timer.totalTime - spent <= 0) {
+        const ds =
+          state.totalTime -
+          state.totalTimeSpent -
+          (Date.now() - state.lastStartAt);
+        if (ds <= 0) {
+          setDistance(0);
           clearInterval(intervalId!);
-          timer.totalTimeSpent = timer.totalTime;
-          timer.isPlay = false;
-          updateTimerById(timer.id, "totalTimeSpent", timer.totalTime as never);
-          set(timer);
+          return;
         }
+        setDistance(ds);
       }, TIME_MOVE);
 
     return () => {
       if (intervalId) clearInterval(intervalId);
     };
-  }, [state.isPlay, state.id, set]);
+  }, [state.isPlay, state.totalTime, state.totalTimeSpent, state.lastStartAt]);
+
+  useEffect(() => {
+    let distance = state.totalTime - state.totalTimeSpent;
+    if (state.isPlay) {
+      distance =
+        state.totalTime -
+        state.totalTimeSpent -
+        (Date.now() - state.lastStartAt);
+    }
+    if (distance <= 0) {
+      setDistance(0);
+      return () => {};
+    }
+    setDistance(distance);
+  }, [state.totalTime, state.totalTimeSpent, state.lastStartAt, state.isPlay]);
 
   return {
     _: {
       timer: state.isPlay,
       distance,
+      distanceConvert,
       totalTime: state.totalTime,
       totalTimeSpent: state.totalTimeSpent,
     },
